ccacab4b6576e03bddcc67aae0be652487230df9
Juan Manuel <manuelsima0305@gmail.com>
2025-01-30 11:40:14 -0600

    Back contraseña

--- routes/autenticacionUsuario.js
+++ routes/autenticacionUsuario.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const autenticacionController = require('../controllers/autenticacionUsuario');
+const autenticacionController = require('../controllers/usuariosController');
 
 // Registrar usuario
 router.post('/register', autenticacionController.registerUser);
