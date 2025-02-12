130f86aa1471b01e4380ddc03b3afaafc23efdbe
Juan Manuel <manuelsima0305@gmail.com>
2025-02-11 18:01:32 -0600

    Nombre de usuario para flutter

--- routes/solicitudesTecnicos.js
+++ routes/solicitudesTecnicos.js
@@ -4,7 +4,7 @@ const solicitudesController = require('../controllers/solicitudesController');
 
 // ✅ Nueva ruta para técnicos: Obtener solicitudes pendientes con info de usuario y servicio
 router.get('/pendientes-tecnicos', solicitudesController.getSolicitudesPendientesTecnicos);
-router.get('/solicitud/:id', solicitudesController.getSolicitudById);
+
 
 
 module.exports = router;
