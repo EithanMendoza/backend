80f7990834138e8fd91758c370745bf7034a1110
Juan Manuel <manuelsima0305@gmail.com>
2025-02-12 20:41:59 -0600

    Profile dinámico para flutter

--- routes/tecnicos.js
+++ routes/tecnicos.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const tecnicosController = require('../controllers/tecnicosController');
-const authMiddleware = require('../middleware/authMiddleware'); // Middleware para verificar usuario autenticado
+const verificarTecnico = require('../middleware/authMiddleware'); // Middleware para verificar usuario autenticado
 
 // Ruta para listar técnicos con paginación
 router.get('/list', tecnicosController.listTecnicos);
 
 // Ruta para obtener el perfil del técnico autenticado
-router.get('/perfil', authMiddleware, tecnicosController.obtenerPerfilTecnico);
+router.get('/perfil', verificarTecnico, tecnicosController.obtenerPerfilTecnico);
 
 module.exports = router;
