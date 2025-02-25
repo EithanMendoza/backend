940e5f5bc046c2141d4d34e799677f78398b2f95
Juan Manuel <manuelsima0305@gmail.com>
2025-02-24 21:00:30 -0600

    Avatar

--- config/gcs.js
+++ config/gcs.js
@@ -1,14 +1,28 @@
 const { Storage } = require("@google-cloud/storage");
-const path = require("path");
 
-const credentials = JSON.parse(process.env.GOOGLE_CLOUD_CREDENTIALS);
+// Verifica que la variable exista
+if (!process.env.GOOGLE_CLOUD_CREDENTIALS) {
+  throw new Error("GOOGLE_CLOUD_CREDENTIALS no está definida.");
+}
+
+let credentials;
+
+try {
+  credentials = JSON.parse(process.env.GOOGLE_CLOUD_CREDENTIALS);
+
+  // Reemplaza los \\n por \n en la private_key
+  credentials.private_key = credentials.private_key.replace(/\\n/g, '\n');
+} catch (error) {
+  console.error("Error al parsear GOOGLE_CLOUD_CREDENTIALS:", error);
+  throw new Error("Credenciales de Google Cloud malformadas.");
+}
 
 // Configuración del bucket
 const storage = new Storage({
-    credentials,
-    projectId: credentials.project_id,
-  });
+  credentials,
+  projectId: credentials.project_id,
+});
 
-const bucket = storage.bucket("airtecs"); // Reemplaza con el nombre de tu bucket
+const bucket = storage.bucket("airtecs");
 
 module.exports = bucket;
