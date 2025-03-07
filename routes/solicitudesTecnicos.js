93205a8ac8a9af76104db08edf3571269856d3b1
Juan Manuel <manuelsima0305@gmail.com>
2025-03-06 21:35:05 -0600

    Notificaciones y Avatar

--- routes/solicitudesTecnicos.js
+++ routes/solicitudesTecnicos.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const solicitudesController = require('../controllers/solicitudesController');
+const verificarTecnico = require('../middleware/tecnicosmiddleware');
 
 // ✅ Nueva ruta para técnicos: Obtener solicitudes pendientes con info de usuario y servicio
 router.get('/pendientes-tecnicos', solicitudesController.getSolicitudesPendientesTecnicos);
-router.get('/pagados-tecnicos', solicitudesController.getSolicitudesPagadas);
+router.get('/pagados-tecnicos', verificarTecnico, solicitudesController.getSolicitudesPagadas);
 
 
 module.exports = router;
